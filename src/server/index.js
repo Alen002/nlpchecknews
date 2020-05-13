72294268a74b8f6fed1370fe9caf72b5cc32db4c
Alen <alechkr@gmail.com>
2020-05-13 13:48:24 +0200

    adds changes to the server file

--- src/server/index.js
+++ src/server/index.js
@@ -8,10 +8,14 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
+    res.sendFile('dist/index.html')
+})
+
+/* app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
-})
+}) */
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
