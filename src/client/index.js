500489ac0f07eb87c3b7227feef7eb7bf40cab60
Alen <alechkr@gmail.com>
2020-05-13 15:54:40 +0200

    adds import of styles in index.js and changed css files to scss

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,11 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
 
 console.log(checkForName);
 
