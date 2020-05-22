b1052203b8c8213a940ade596eaa582203b58832
Alen <alechkr@gmail.com>
2020-05-22 17:21:39 +0200

    added Aylien API and several npm packages

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,45 @@
+const dotenv = require('dotenv')
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const morgan = require('morgan');
+const aylienTextAPI = require('aylien_textapi')
+const bodyParser = require('body-parser');
+
+
+// Initialize the Aylien API and make a request
+var textapi = new aylienTextAPI({
+    application_id: process.env.api_id,  // keys are from the .env file
+    application_key: process.env.api_key // keys are from the .env file
+  });
 
 const app = express()
+app.use(morgan('short'));  // short or combines can be used as attributes
+app.use(express.static('dist'));
 
-app.use(express.static('dist'))
+// middleware - body-parser
+app.use(bodyParser.urlencoded({extended: false})); 
+app.use(bodyParser.json());
 
 console.log(__dirname)
 
 app.get('/', (req, res) => {
     res.sendFile('dist/index.html')
+});
+
+app.get('/api', (req, res) => {
+    textapi.sentiment({
+        'text': 'Today is a great day'   // text from the user entered in the form of index.html
+      }, function(error, response) {
+        if (error === null) {
+          res.send(response);
+        } else console.log('API not working');
+    });
 })
+
+
 
 /* app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -25,3 +54,4 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
--- src/server/mockAPI.js
+++ src/server/mockAPI.js
@@ -1,7 +1,8 @@
 let json = {
     'title': 'test json response',
     'message': 'this is a message',
-    'time': 'now'
+    'time': 'now',
+    
 }
 
 module.exports = json
