292053487be64357b8436b288d8777424bbcfbf8
Alen <alechkr@gmail.com>
2020-05-23 20:51:49 +0200

    formHandler is now working. Output library was mising in webpack.prod

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ const mockAPIResponse = require('./mockAPI.js')
 const morgan = require('morgan');
 const aylienTextAPI = require('aylien_textapi')
 const bodyParser = require('body-parser');
-/* const cors = require('cors'); */
+const cors = require('cors');
 
 
 // Initialize the Aylien API and make a request
@@ -18,7 +18,7 @@ var textapi = new aylienTextAPI({
 
 const app = express();
 app.use(express.json());
-/* app.use(cors()); */
+app.use(cors());
 
 app.use(morgan('short'));  // short or combines can be used as attributes
 app.use(express.static('dist'));
@@ -28,22 +28,30 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 console.log(__dirname)
-
 app.get('/', (req, res) => {
     res.sendFile('dist/index.html')
 });
 
-app.post('/api', (req, res) => {
-  
+// The POST request for the API data
+app.post('/api', (req, res) => {  
   textapi.sentiment(
-    req.body, 
+    req.body, // json file is received as {"text": "Your text"} -> testing via postman
     function(error, response) {
     if (error === null) {
-      res.send(response);
-      console.log(response);
-    }
+      res.send(response); // response is the output data of the API 
+      console.log(response); // Display the response on the server
+    } else console.log('API is not working');
   });
 });
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+  res.send(mockAPIResponse)
+});
+
 
 
 /* app.get('/', function (req, res) {
@@ -51,12 +59,6 @@ app.post('/api', (req, res) => {
     res.sendFile(path.resolve('src/client/views/index.html'))
 }) */
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+// designates which port the app will listen to for incoming requests
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
 
